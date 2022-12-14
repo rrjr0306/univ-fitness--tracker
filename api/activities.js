b46c499cfb1caf1a1b3b79a8c21be0b494f4ffca
Colin's Laptop <colinkaniff1994@gmail.com>
2022-12-13 21:09:23 -0500

    FEAT activitiesDB start apiActivities

--- api/activities.js
+++ api/activities.js
@@ -1,12 +1,49 @@
+/* eslint-disable no-useless-catch */
 const express = require('express');
-const router = express.Router();
+const activitiesRouter = express.Router();
+const {getAllActivities,
+    getActivityById,
+
+    createActivity,
+    updateActivity
+} = require('../db')
 
 // GET /api/activities/:activityId/routines
 
 // GET /api/activities
+activitiesRouter.get('/', (req, res) => {
+    const allActivities = getAllActivities();
+
+    console.log("HELP", allActivities)
+
+    const activities = allActivities.filter((activity) => {
+        return activity;
+    })
+
+    res.send({
+        activities
+    })
+})
 
 // POST /api/activities
+activitiesRouter.post('/', (req, res) => {
+    const {name, description} = req.body;
 
+    const activityData = {};
+
+    try {
+        activityData.name = name;
+        activityData.description = description;
+        
+        const activity = createActivity(activityData);
+
+        
+        res.send({activity})
+
+    } catch(error) {
+        throw error;
+    }
+})
 // PATCH /api/activities/:activityId
 
-module.exports = router;
+module.exports = activitiesRouter;
