3065fad9384e62a953b89da0daa36b0ed4d0ffe4
Aaliyah Augustus <aaliyahaugustus@yahoo.com>
2022-12-23 12:37:55 -0600

    installed react and created routes

---
+++ src/components/Routines.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Routines = () => {
+    return (
+        <>
+        <h1>
+            Routines
+        </h1>
+       
+        </>
+    )
+}
+
+export default Routines;
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export {default as NavBar} from './Navbar';
+export {default as Home} from './Home';
+export {default as Activities} from './Activities';
+export {default as Routines} from './Routines';
+export {default as Myroutines} from './Myroutines';
