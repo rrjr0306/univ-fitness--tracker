353178c83f1c97abb1f29037d480321ee120fa7d
rrjr0306 <rrjr0306@gmail.com>
2022-12-29 22:04:22 -0500

    frontend: started routines

--- src/App.js
+++ src/App.js
@@ -1,32 +1,123 @@
-import React from "react";
-import {Home, Activities, Routines, Myroutines} from "./components";
-import {Route, Switch} from "react-router-dom";
+// import React from "react";
+// import {Home, Activities, Routines, Myroutines} from "./components";
+// import {Route, Switch} from "react-router-dom";
 
-// const BASE_URL = ""
+// // const BASE_URL = ""
+
+// const App = () => {
+
+// return (
+// <div>
+//     <NavBar />
+
+//     <Switch>
+//         <Route exact path="/" component={Home}/>
+//         <Route path="/Activities" component={Activities}/>
+//         <Route path="/Routines" component={Routines}/>
+//         <Route path="/Myroutines" component={Myroutines}/>
+//     </Switch>
+
+// </div>
+
+// )
+// }
+
+
+// export default App;
+
+
+
+import React, { useState, useEffect } from "react";
+import { Home, Activities, Routines, Myroutines } from "./components";
+import { Route, Routes, Link, useNavigate } from "react-router-dom";
+import { getRoutines, fetchGuest } from "./api/api";
+import "./App.css";
 
 const App = () => {
+  const [routines, setRoutines] = useState([]);
+  const [token, setToken] = useState(
+    window.localStorage.getItem("token") || null
+  );
+  const [guest, setGuest] = useState(null);
+  
+  const navigate = useNavigate();
 
-return (
-<div>
-    <NavBar />
+  useEffect(() => {
+    const getRoutines = async () => {
+        const {error, routines} = await getRoutines(token);
 
-    <Switch>
-        <Route exact path="/" component={Home}/>
-        <Route path="/Activities" component={Activities}/>
-        <Route path="/Routines" component={Routines}/>
-        <Route path="/Myroutines" component={Myroutines}/>
-    </Switch>
+        if (error) {
+            console.error(error);
+        }
+        setRoutines(routines);
+      };
+    getRoutines();
+  }, [token]);
 
-</div>
+  useEffect(() => {
+    if (token) {
+        const getGuest = async () => {
+            const { username } = await fetchGuest(token);
+            // console.log("username", username);
+            setGuest(username)
+        };
+        getGuest();
+    }
+  }, [token]);
 
-)
-}
+  useEffect(() => {
+    if (token) {
+        window.localStorage.setItem("token", token);
+    } else {
+        window.localStorage.removeItem("token");
+    }    
+  }, [token]);
 
+  const logOut = () => {
+    setToken("");
+    setGuest(null);    
+    navigate("/");
+  }
 
+  return (
+    <div className="container">
+      <nav className="ui secondary menu">
+        <Link className="item" to="/">
+          Home
+        </Link>
+        <Link className="item" to="/routines">
+          Routines
+        </Link>
+        <div className="right menu">
+          {token ? (
+            <button style={{fontFamily: 'itc-benguiat, serif'}} onClick={logOut} className="item">Log Out</button>
+          ) : (
+            <>
+              <Link className="item" to="/account/login">
+                Log In
+              </Link>
+              <Link className="item" to="/account/register">
+                Sign Up
+              </Link>
+            </>
+          )}
+        </div>
+      </nav>
 
-
-
-
-
+      <Routes>
+        <Route exact path="/" element={<Home guest={guest} />} />
+        <Route path="/routines/create" element={<CreateRoutineForm token={token} setRoutines={setRoutines} />} />
+        <Route path="/routines/:routineId" element={<RoutineDetails routines={routines} token={token} getRoutines={getRoutines}/>} />
+        <Route path="/routines" element={<Routines routines={routines} token={token} setRoutines={setRoutines} />} />
+        <Route
+          path="/account/:action"
+          element={<AccountForm setToken={setToken} />}
+        />
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
+
+
--- src/components/Routines.js
+++ src/components/Routines.js
@@ -1,14 +1,68 @@
-import React from "react";
+// import React from "react";
+
+// const Routines = () => {
+//     return (
+//         <>
+//         <h1>
+//             Routines
+//         </h1>
+       
+//         </>
+//     )
+// }
+
+// export default Routines;
+
+
+
+
+import React, { useState, useEffect } from "react";
+const baseUrl = 'https://fitness-tracker-z419.onrender.com/api'
+
+export const getRoutines = async () => {
+    const url = `${baseUrl}/routines`;
+    try{
+        const response = await fetch(url, {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json',
+            }           
+        })    
+    const json = await response.json()    
+    return json;
+} catch (error){console.error(error, "Something's wrong with getting routines!")}
+} 
 
 const Routines = () => {
-    return (
-        <>
-        <h1>
-            Routines
-        </h1>
-       
-        </>
-    )
-}
+    const [allRoutines, setAllRoutines] = useState({});
+    const [routine, setRoutine] = useState([])
+    useEffect(async() => {
+      const gettingRoutines = await getRoutines();        
+        setAllRoutines(gettingRoutines);
+        setRoutine(gettingRoutines)   
+    }, []);
+    console.log(routine)
+    
+  return (
+    <div>
+        {routine.map(content =>
+            <div key={content.id}>
+               <h2>Routine Creator - {content.creatorName}</h2>
+                <p>Name - {content.name}</p>
+                <p>Goal - {content.goal}</p>
+                <div>
+                    {content.activities.map(activity =>
+                        <div key={activity.id}>
+                            <h3>Activity name - {activity.name}</h3>
+                            <p>Activity discription - {activity.description}</p>
+                            <p>Duration and Count - {activity.duration} , {activity.count}</p>
+                        </div>
+                        )}
+                </div>                 
+            </div>
+            )}
+    </div>
+  );
+};
 
 export default Routines;
