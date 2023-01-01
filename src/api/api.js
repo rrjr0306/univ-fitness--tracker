523d37ccd98bcb6447ecf4764846ce9a6d025a91
rrjr0306 <rrjr0306@gmail.com>
2023-01-01 16:30:32 -0500

    Finalize MyRoutines, add to api.js

--- src/api/api.js
+++ src/api/api.js
@@ -119,3 +119,42 @@ export const getRoutines = async () => {
     return json;
 } catch (error){console.error(error, "Something's wrong with getting routines!")}
 } 
+
+export const createRoutine = async (name, goal, isPublic, token) => {    
+    const url = `${BASE_URL}/routines`;
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify({
+            name,
+            goal,
+            isPublic,
+        })
+    });
+    const result = await response.json();
+    return result;
+};
+
+export const getUserRoutines = async() => {
+    const username = localStorage.getItem('username');
+    const url = `${BASE_URL}/MyRoutines`
+    const token = localStorage.getItem('token')
+
+    try {
+        const response = await fetch(url, {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            }       
+        });
+
+        const result = await response.json();
+        return result
+    } catch (error) {
+        console.error("Error getting user's routines", error)
+    }
+}
