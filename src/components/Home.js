cda916fc336060260a58655e03e629508d08bd7b
Aaliyah Augustus <aaliyahaugustus@yahoo.com>
2023-01-02 20:02:40 -0600

    styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
-import {Link} from "react-router-dom"
+import {Link} from "react-router-dom";
+import './Home.css';
 
 
 const Home = ({username, token}) => {
@@ -10,6 +11,7 @@ const Home = ({username, token}) => {
             <div>
                 <h2>Please <Link to="AccountForm/login">Log In</Link></h2>
                 <h3>Not a member? <Link to="AccountForm/register">Sign Up!</Link></h3>
+                <img src= 'https://images.unsplash.com/photo-1517836357463-d25dfeac3438?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'></img>
             </div>
             )}
         </div>
