cb4f2684d6e0d1c9e763caea57ca04766c6bc201
Colin's Laptop <colinkaniff1994@gmail.com>
2023-01-02 19:05:28 -0500

    activities

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 export {default as AccountForm} from './AccountForm';
 export {default as Home} from './Home';
 export {default as Activities} from './Activities';
+export {default as ActivityItem} from './ActivityItem'
 export {default as Routines} from './Routines';
 export {default as Myroutines} from './Myroutines';
 export {default as CreateActivity } from './CreateActivity';
